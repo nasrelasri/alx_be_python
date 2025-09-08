7025d882a1b8b3c57b968a1bbb2a0778d90864b4
Nasr El Asri <nasr.asri10@gmail.com>
2025-09-08 17:27:15 +0100

    Create basic arithmetic exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 13
+number2 = 10
+
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+quotient = number1 / number2
+
+print(f"Addition of {number1} and {number2} is", sum_result)
+print(f"Subtraction of {number1} and {number2} is", difference)
+print(f"Multiplication of {number1} and {number2} is", product)
+print(f"Division of {number1} and {number2} is", quotient)
